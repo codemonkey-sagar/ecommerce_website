db459d6d1ea00808880fd48451ccba824c061b02
codemonkey-sagar <fmtsagar@gmail.com>
2024-08-07 13:33:14 -0400

    Adding product controller to refactor code

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,34 +1,12 @@
 import express from 'express';
-import mongoose from 'mongoose';
-import Product from '../models/productModel.js';
+import {
+  getProducts,
+  getProductById,
+} from '../controllers/productController.js';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.json(products);
-  } catch (error) {
-    res.status(500).json({ message: 'Error fetching products' });
-  }
-});
-
-router.get('/:id', async (req, res) => {
-  try {
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-      return res.status(400).json({ message: 'Invalid Product ID' });
-    }
-
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404).json({ message: 'Product not found' });
-    }
-  } catch (error) {
-    res.status(500).json({ message: 'Error fetching product' });
-  }
-});
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
