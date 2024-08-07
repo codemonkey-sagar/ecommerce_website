0ac01addb93b4afcb1b12ef356456f4029643273
codemonkey-sagar <fmtsagar@gmail.com>
2024-08-07 16:23:43 -0400

    Adding redux for produts

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './store.js';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
 import Home from './pages/Home/Home.jsx';
@@ -25,6 +27,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,49 +1,31 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
 import Product from '../../components/Product/Product';
+import { useGetProductsQuery } from '../../slices/productsApiSlice';
 import './Home.css';
 
 const Home = () => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await axios.get('http://localhost:8000/api/products');
-        setProducts(response.data);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchProducts();
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
+  const { data: products, isLoading, error } = useGetProductsQuery();
 
   return (
     <div className='home'>
       <div className='home__container'>
-        <h1>Latest Products</h1>
-        <div className='products'>
-          {Array.isArray(products) ? (
-            products.map((product) => (
-              <Product key={product._id} product={product} />
-            ))
-          ) : (
-            <div>No products available</div>
-          )}
-        </div>
+        {isLoading ? (
+          <h1>Loading...</h1>
+        ) : error ? (
+          <div>{error.data?.message || error.error}</div>
+        ) : (
+          <>
+            <h1>Latest Products</h1>
+            <div className='products'>
+              {Array.isArray(products) && products.length > 0 ? (
+                products.map((product) => (
+                  <Product key={product._id} product={product} />
+                ))
+              ) : (
+                <div>No products available</div>
+              )}
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
