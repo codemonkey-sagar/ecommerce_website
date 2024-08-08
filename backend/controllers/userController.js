93d4ce597b0abaa615f24c533330a65549a5b535
codemonkey-sagar <fmtsagar@gmail.com>
2024-08-08 14:35:47 -0400

    User registration and code refactor

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,24 +1,13 @@
 import asyncHandler from '../middleware/asyncHandler.js';
+import generateToken from '../utils/generateToken.js';
 import User from '../models/userModel.js';
-import jwt from 'jsonwebtoken';
 
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
 
   if (user && user.matchPassword(password)) {
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: '30d',
-    });
-
-    // SET JWT as HTTP-Only cookie
-    res.cookie('jwt', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development',
-      sameSite: 'strict',
-      maxAge: 30 * 24 * 60 * 60 * 1000,
-    });
-
+    generateToken(res, user._id);
     res.json({
       _id: user._id,
       name: user.name,
@@ -34,11 +23,43 @@ const authUser = asyncHandler(async (req, res) => {
 });
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.send('register user');
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    generateToken(res, user._id);
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send('Logout user');
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: 'Logout successfully' });
 });
 
 const getUserprofile = asyncHandler(async (req, res) => {
